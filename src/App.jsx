bd7df45463376f12fecc4b404c13571614ee43dd
Vaibhav Sharma <vaibhavsharma6060@gmail.com>
2025-07-16 14:18:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom'
-import './App.css'
 import Pokedex from './components/Pokedex/Pokedex'
 import PokemonDetails from './components/PokemonDetails/PokemonDetails'
 
