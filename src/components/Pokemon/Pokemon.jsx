232de6e458ba12b92a27dd5a32c2d2b2feca8219
Vaibhav Sharma <vaibhavsharma6060@gmail.com>
2025-08-04 15:12:37 +0530

    Update Pokemon.jsx

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -1,16 +1,14 @@
-// CSS imports
 import './Pokemon.css';
-import { Link } from 'react-router-dom';
-function Pokemon({ name, url, id }) {
-    return (
-        <Link to={`/pokemon/${id}`} className='pokemon-wrapper'> 
-            <div className='pokemon'>
-                    <div className='pokemon-name'>{name}</div>
-                    <img className='pokemon-image' src={url} />
-            </div>
-        </Link>
 
-    )
+function Pokemon({ name, url }) {
+    return (
+        <div className='pokemon-wrapper'>
+            <div className='pokemon'>
+                <div className='pokemon-name'>{name}</div>
+                <img className='pokemon-image' src={url} />
+            </div>
+        </div>
+    );
 }
 
 export default Pokemon;
